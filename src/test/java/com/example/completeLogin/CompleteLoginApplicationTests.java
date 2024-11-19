d2a5732ff181cc673f11d639385cdc69e2d06be3
Vornicu <petrut.anddrei@yahoo.com>
2024-11-19 20:35:02 +0200

    Single commit

---
+++ src/test/java/com/example/completeLogin/CompleteLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completeLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
